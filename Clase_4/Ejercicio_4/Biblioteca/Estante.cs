1e61121d1ac23513cf19966eb2e954a15723dbea
Leandro AG <leanndro.ps4@gmail.com>
2022-09-05 22:36:27 -0300

    estanteria

---
+++ Clase_4/Ejercicio_4/Biblioteca/Estante.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacion;
+
+        private Estante(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+        public Estante(int capacidad, int ubicacion) 
+            : this(capacidad)
+        {
+            this.ubicacion = ubicacion;
+        }
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+      
+        //sobrecarga 
+        public static bool operator ==(Estante estante, Producto producto)
+        {
+            for (int i = 0; i < estante.productos.Length; i++)
+            {
+                if (estante.productos[i] == producto)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool operator !=(Estante estante, Producto producto)
+        {
+            return !(estante == producto);
+        }
+        public static bool operator +(Estante estante, Producto producto)
+        {
+            if (estante != producto)
+            {
+                for (int i = 0; i < estante.productos.Length; i++)
+                {
+                    if (estante.productos[i] is null)
+                    {
+                        estante.productos[i] = producto;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public static Estante operator -(Estante estante, Producto producto)
+        {
+            for (int i = 0; i < estante.productos.Length; i++)
+            {
+                if (estante.productos[i] == producto)
+                {
+                    estante.productos[i] = null;
+                    break;
+                }
+            }
+            return estante;
+        }
+
+        public static string MostrarEstante(Estante estante)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"ubicacion: {estante.ubicacion} \n");
+
+            for (int i = 0; i < estante.productos.Length; i++)
+            {
+                if (!(estante.productos[i] is null))
+                {
+                    sb.AppendLine(Producto.MostrarProducto(estante.productos[i]));
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Clase_4/Ejercicio_4/Biblioteca/Producto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigoDeBarra, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigoDeBarra;
+            this.precio = precio;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+     
+
+        public static explicit operator string(Producto producto)
+        {
+            return producto.codigoDeBarra;
+        }
+        public static bool operator ==(Producto producto1, Producto producto2)
+        {
+            if (!(producto1 is null) || (producto2 is null))
+            {
+                return producto1.marca == producto2.marca && producto1.codigoDeBarra == producto2.codigoDeBarra;
+            }
+            return false;
+            
+        }
+
+        public static bool operator !=(Producto producto1, Producto producto2)
+        {
+            return !(producto1.marca == producto2.marca && producto1.codigoDeBarra == producto2.codigoDeBarra);
+        }
+
+        public static bool operator ==(Producto producto, string marca)
+        {
+ 
+            return producto.marca == marca;
+        }
+        public static bool operator !=(Producto producto, string marca)
+        {
+            return !(producto.marca == marca);
+        }
+
+        public static string MostrarProducto(Producto producto)
+        {
+
+            return String.Format($"marca {producto.marca} codigo {producto.codigoDeBarra} precio {producto.precio} ");
+        }
+    }
+}
